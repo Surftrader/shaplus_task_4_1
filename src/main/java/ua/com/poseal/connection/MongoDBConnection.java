dfb9ccdf833a27469d34ebf1bdb05cf66d6cc7a6
Sergiy Poddubka <poseal@gmail.com>
2023-07-18 11:50:37 +0300

    add certificate

--- src/main/java/ua/com/poseal/connection/MongoDBConnection.java
+++ src/main/java/ua/com/poseal/connection/MongoDBConnection.java
@@ -6,6 +6,7 @@ import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
 import com.mongodb.client.MongoDatabase;
 
+import javax.print.attribute.standard.JobOriginatingUserName;
 import java.util.Properties;
 
 import static ua.com.poseal.App.logger;
@@ -13,6 +14,8 @@ import static ua.com.poseal.App.logger;
 public class MongoDBConnection implements Connection {
     public static final String URL = "url";
     public static final String DATABASE = "database";
+    public static final String USERNAME = "username";
+    public static final String PASSWORD = "password";
     public static final MongoDBConnection instance = getInstance();
 
     private MongoClient mongoClient;
@@ -29,11 +32,29 @@ public class MongoDBConnection implements Connection {
         // Creating a MongoDB Client
         MongoDatabase database = null;
         try {
-            ConnectionString connectionString = new ConnectionString(properties.getProperty(URL));
-            MongoClientSettings settings = MongoClientSettings.builder()
-                    .applyConnectionString(connectionString)
-                    .build();
-            mongoClient = MongoClients.create(settings);
+//            ConnectionString connectionString = new ConnectionString(properties.getProperty(URL));
+
+            String template = "mongodb://%s:%s@%s/shapp_db?ssl=true&replicaSet=rs0&readpreference=%s";
+            String username = properties.getProperty(USERNAME);
+            String password = properties.getProperty(PASSWORD);
+            String clusterEndpoint = "docdb-2023-07-17-17-11-32.cqqiakrcxslc.eu-west-3.docdb.amazonaws.com:27017";
+            String readPreference = "secondaryPreferred";
+            String connectionString = String.format(template, username, password, clusterEndpoint, readPreference);
+
+            String truststore = properties.getProperty("javax.net.ssl.trustStore");
+            String truststorePassword = properties.getProperty("javax.net.ssl.trustStorePassword");
+
+            System.setProperty("javax.net.ssl.trustStore", truststore);
+            System.setProperty("javax.net.ssl.trustStorePassword", truststorePassword);
+
+            MongoClient mongoClient = MongoClients.create(connectionString);
+
+
+
+//            MongoClientSettings settings = MongoClientSettings.builder()
+//                    .applyConnectionString(connectionString)
+//                    .build();
+//            mongoClient = MongoClients.create(settings);
             database = mongoClient.getDatabase(properties.getProperty(DATABASE));
 
         } catch (Exception e) {
